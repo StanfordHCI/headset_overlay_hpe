f2beb2fb96be3116ce6d6aca721268fab2eb5dfe
Jackie Yang <jackie@jackieyang.me>
2021-03-30 15:35:13 -0700

    update kubeflow

--- docker/videopose_gen.py
+++ docker/videopose_gen.py
@@ -9,7 +9,6 @@ from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Generate data for videopose')
 parser.add_argument('--git_rev', type=str)
-parser.add_argument('--update_data', type=str)
 parser.add_argument('--resnet_model', type=str)
 parser.add_argument('--mode', type=str)
 parser.add_argument('--name', type=str)
@@ -30,40 +29,29 @@ DATA_DRIVE = "/data/"
 DATA_RAW = os.path.join(DATA_DRIVE, "raw")
 DATA_RAW_H36M = os.path.join(DATA_RAW, "h36m")
 DATA_RAW_MPI = os.path.join(DATA_RAW, "mpi")
-DATA_RAW_H36M_S3 = "s3://geniehai/jackiey/videopose3d/raw/h36m.tar.bz2"
-DATA_RAW_MPI_S3 = "s3://geniehai/jackiey/videopose3d/raw/mpi.tar.bz2"
 
 
 def checkout_repo(git_rev: str):
     os.system(f"cd {REPO_DIR} && git pull && git checkout {git_rev}")
 
-def pull_data():
-    if not os.path.exists(DATA_DIR) or args.update_data == 'true':
-        if args.update_data == 'true':
-            shutil.rmtree(DATA_DIR)
-        print("pulling data")
-        os.system(f"aws s3 cp {DATA_S3} {DATA_DRIVE}")
-        os.system(f"cd {DATA_DRIVE} && pbzip2 -dc data.tar.bz2 | tar x")
 
-    if args.update_data == 'true':
-        shutil.rmtree(MODELS_DIR)
-    if not os.path.exists(MODELS_DIR) or args.update_data == 'true':
-        print("pulling models")
-        os.system(f"aws s3 cp {MODELS_S3} {DATA_DRIVE}")
-        os.system(f"cd {DATA_DRIVE} && pbzip2 -dc models.tar.bz2 | tar x")
-
-def gen_3d():
-    train_process = subprocess.Popen(
+def gen_3d_h36m():
+    gen_process = subprocess.Popen(
         f"cd {REPO_DIR} &&"
-        f"python3 pose_estimation/train.py "
-        f"--cfg experiments/{args.config} "
-        f"--log {LOG_DIR} "
+        f"python3 data/prepare_data_h36m.py "
+        f"--from-source-cdf {DATA_RAW_H36M} "
         f"{args.additional_args}",
         shell=True
     )
+    gen_process.wait()
+
+
+def sync_s3():
+    os.system(f"aws s3 sync --no-progress data_* {S3_OUTPUT_DIR}")
 
 
 if __name__ == '__main__':
     checkout_repo(args.git_rev)
-
-
+    if args.mode == "3d_h36m":
+        gen_3d_h36m()
+        sync_s3()
--- docker/videopose_pull.py
+++ docker/videopose_pull.py
@@ -26,8 +26,11 @@ def pull_any_data(data_dir, data_s3, force_update):
     parent_data_dir = os.path.dirname(data_dir)
     file_name = data_s3.rsplit('/', 1)
     print(f"pulling data from {data_s3}({file_name}) to {parent_data_dir} for {data_dir}")
+    if not os.path.isdir(parent_data_dir):
+        os.mkdir(parent_data_dir)
     os.system(f"aws s3 cp {data_s3} {parent_data_dir}")
     os.system(f"cd {parent_data_dir} && tar -xzvf {file_name} && rm {file_name}")
+    os.system(f"df -h")
 
 
 def pull_data():
